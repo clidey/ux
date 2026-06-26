f7ddbad2359d26ce44e84be2b69e071046512bf6
Hemang Kandwal <h@clidey.com>
2026-06-26 01:48:49 +0100

    feat(ui): fix up table row

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -560,7 +560,7 @@ function TableRow({ className, style, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
+        "hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors table table-auto w-full",
         className
       )}
       style={style}
